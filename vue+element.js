d27411aad3c1d2ea2749f3f5e133eb3d2ab89c10
L-Congratulations <15188961373@163.com>
2017-04-18 15:10:37 +0800

    Update vue+element.js

--- vue+element.js
+++ vue+element.js
@@ -6,4 +6,19 @@
   import ElementUI from 'element-ui'
   import 'element-ui/lib/theme-default/index.css'
   Vue.use(ElementUI)
-  以上代码便完成了 Element 的引入。需要注意的是，样式文件需要单独引入。
+3、（报错是由于我们引入了index.css这个 CSS 文件，但是 webpack 打包的时候无法识别并转换成 js，所以就需要配置才能读取 css 和字体文件，
+    运行命令安装下面三个东西(如果之前安装过就不需要了)）
+    在命令行中输入：
+    npm install style-loader --save-dev
+    npm install css-loader --save-dev
+    npm install file-loader --save-dev
+4、在 webpack.config.js 中的 loaders 数组加入以下配置，记得该加逗号的地方加逗号!
+  {
+    test: /\\.css$/,
+    loader: "style!css"
+  },
+  {
+      test: /\\.(eot|woff|woff2|ttf)([\\?]?.*)$/,
+      loader: "file"
+  },
+  
